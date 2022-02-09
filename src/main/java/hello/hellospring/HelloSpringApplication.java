f48d07e1439abe9aebcef17debf994fb906e0c1a
KiSeungMin <ksm011212@gmail.com>
2022-02-09 21:27:39 +0900

    2/9

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,10 +3,13 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 스프링부트는 내장 서버인 톰캣을 실행시킨다.
+// 스프링부트와 관련된 라이브러리를 쓰면 의존관계에 의해 다른 라이브러리까지 실행된다.
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
